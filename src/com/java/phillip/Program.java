c6cfef453bc2a1ec58522669364c72dc08dddeb4
humblebumble90 <hyngsuki@gmail.com>
2021-12-10 13:56:23 -0500

    Third commit

--- src/com/java/phillip/Program.java
+++ src/com/java/phillip/Program.java
@@ -1,10 +1,11 @@
 package com.java.phillip;
 
 public class Program {
-
+private static String _name = "";
 	public static void main(String[] args) {
 		System.out.println("Program is running.....");
-
+		_name = "Phillip";
+		System.out.println(_name);
 	}
 
 }
